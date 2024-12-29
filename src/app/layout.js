e5b15add8931a80205644d23b8765b75bf7abf96
Samiul Kabir <samkabir26@gmail.com>
2024-12-29 19:34:03 +0600

    more fixes 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <AppsProvider>
           {children}
         </AppsProvider>
