bcaa3a02431c3b81dd9bbe03210d9ab0ec7d7b09
Samiul Kabir <samkabir26@gmail.com>
2024-12-29 20:57:43 +0600

    asd2

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ import AllProducts from "@/components/WebPages/AllProductsPage/AllProducts";
 export default function Home() {
   return (
     <div className="customContainer">
-      {/* <Navbar /> */}
-      <AllProducts />
+      <Navbar />
+      {/* <AllProducts /> */}
     </div>
   );
 }
