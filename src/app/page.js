4323069342baf807b6219c13b250fabfd925c876
Samiul Kabir <samkabir26@gmail.com>
2024-12-29 21:31:48 +0600

    Add 5

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 "use client";
 
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function Home() {
   return (
     <div className="customContainer">
-      dffdsfs
+      <Navbar />
     </div>
   );
 }
