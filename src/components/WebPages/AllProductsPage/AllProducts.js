994a2be9695e7fa02fa8d742787df9295a45f700
Samiul Kabir <samkabir26@gmail.com>
2024-12-29 21:53:01 +0600

    Test 1

--- src/components/WebPages/AllProductsPage/AllProducts.js
+++ src/components/WebPages/AllProductsPage/AllProducts.js
@@ -10,21 +10,21 @@ const AllProducts = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const getAllProducts = async () => {
-        setLoading(true);
-        const { success, data, message } = await fetchAllProducts();
-        if (success) {
-            setProducts(data);
-        } else {
-            setError(message);
-        }
-        setLoading(false);
-    };
-    useEffect(() => {
-        if (typeof window !== "undefined"){
-            getAllProducts();
-        }
-    }, []);
+    // const getAllProducts = async () => {
+    //     setLoading(true);
+    //     const { success, data, message } = await fetchAllProducts();
+    //     if (success) {
+    //         setProducts(data);
+    //     } else {
+    //         setError(message);
+    //     }
+    //     setLoading(false);
+    // };
+    // useEffect(() => {
+    //     if (typeof window !== "undefined"){
+    //         getAllProducts();
+    //     }
+    // }, []);
     
   return (
     <div className='customGridRange my-5'>
@@ -35,11 +35,12 @@ const AllProducts = () => {
         ) : error ? (
             <div className='flex justify-center'>{error}</div>
         ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5  gap-2'>
-                {products?.products?.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
+            // <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5  gap-2'>
+            //     {products?.products?.map((product) => (
+            //         <ProductCard key={product.id} product={product} />
+            //     ))}
+            // </div>
+            <></>
         )}
     </div>
   )
