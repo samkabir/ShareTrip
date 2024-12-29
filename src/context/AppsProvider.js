8f7780c67a5835256dff0fd667d4693d4daf098b
Samiul Kabir <samkabir26@gmail.com>
2024-12-29 20:36:09 +0600

    idnot know

--- src/context/AppsProvider.js
+++ src/context/AppsProvider.js
@@ -6,11 +6,11 @@ import { FavouriteProvider } from "./FavouriteContext";
 
 const AppsProvider = ({ children }) => {
   return (
-    <CartProvider>
-      <FavouriteProvider>
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-      </FavouriteProvider>
-    </CartProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <CartProvider>
+        <FavouriteProvider>{children}</FavouriteProvider>
+      </CartProvider>
+    </Suspense>
   );
 };
 
